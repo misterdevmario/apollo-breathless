af7971ed957a9fcfbab1476de2a7fdc8458a6e9e
Mario <mariorodriguez1110@gmail.com>
2024-05-19 23:37:10 -0600

    Reload set at 2 minutes

--- client/app/page.js
+++ client/app/page.js
@@ -229,7 +229,7 @@ export default function Page() {
   useEffect(() => {
     const reload = setInterval(() => {
       window.location.reload();
-    }, 1800000);
+    }, 120000);
     return () => {
       clearInterval(reload);
     };
